0c0c2276bbd458924f5458a2265e8750f000b4b7
Wanessa Guedes <wanessa.o.guedes@gmail.com>
2022-05-11 19:33:39 -0300

    Adding basic config to start the back

---
+++ db.js
@@ -0,0 +1,13 @@
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+let db = null;
+const mongoClient = new MongoClient(process.env.MONGO_URI);
+
+await mongoClient.connect();
+
+db = mongoClient.db("nova");
+
+export default db;
