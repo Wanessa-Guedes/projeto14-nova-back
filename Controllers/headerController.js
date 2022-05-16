70dbb2986685c4a1036ca633dd15603201470b50
Wanessa Guedes <wanessa.o.guedes@gmail.com>
2022-05-16 10:24:33 -0300

    fix: controllers problems

--- Controllers/headerController.js
+++ Controllers/headerController.js
@@ -10,11 +10,10 @@ export async function deleteLogOut(req, res){
 
     const { authorization } = req.headers;
     const token = authorization?.replace('Bearer ', '');
-    console.log(token)
     if(!token) return res.status(401).send("Verifique se você estava logado.");
 
     const sessions = await db.collection("sessions").findOne({token});
-    const user = await db.collection("users").findOne({_id: sessions.userId});
+    const user = await db.collection("users").findOne({_id: ObjectId(sessions.userId)});
 
     await db.collection("sessions").deleteOne({token});
     res.status(200).send("Sessão finalizada!");
